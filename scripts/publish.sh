f2fb7fd3f6ae047799592ab6481ec8e86c388542
Edward Sherrill <teddy.sherrill@gmail.com>
2023-07-13 23:03:39 -0400

    Don't push to git

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,2 @@
-# npm version -m "Release v%s" --tag-version-prefix="v" $1
-# git push && git push --tags
-
-echo "Skipping publish-cli.sh"
+npm version -m "Release v%s" --tag-version-prefix="v" $1
+git push && git push --tags
